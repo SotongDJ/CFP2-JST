dab2a4851dc5d77c2b89aadf3de5a2e9862ca96d
David Soh <ln@trth.nl>
2024-04-11 02:01:49 +0800

    feat: use CFP2-FST as template

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CFP2-JST
+# CFP2-JST (Work in progress)
 CuttleFish podcast player for "唐陽雞酒屋" podcast (Unofficial podcast player)
---
+++ docs/jst.js
@@ -0,0 +1,7 @@
+final_title_str="CFP2-JST 《唐陽雞酒屋》播放室";
+final_root_path="https://xn--r09h.xn--2os22eixx6na.xn--kpry57d/";
+final_artist_str="唐綺陽";
+show_apple=true;
+show_google=false;
+show_spotify=true;
+show_youtube=false;
