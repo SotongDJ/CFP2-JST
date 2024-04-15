91f8a859b702f5b79f62cc0c4cdd7cb8abeeb960
David Soh <ln@trth.nl>
2024-04-15 16:52:11 +0000

    refactor: more modularize and change button list

--- docs/jst.js
+++ docs/jst.js
@@ -1,6 +1,20 @@
 final_title_str="CFP2-JST 《唐陽雞酒屋》播放室";
 final_root_path="https://xn--r09h.xn--2os22eixx6na.xn--kpry57d/";
 final_artist_str="唐綺陽";
+
+document.getElementById("aboutA").href="https://github.com/SotongDJ/CFP2-JST";
+document.getElementById("emailA").href="mailto:github@stdj.me?subject=JST 回饋信件&body=內容類型：（問題？建議？）%0A細節：（越詳細越好）";
+document.getElementById("infoA").href="https://github.com/SotongDJ/CFP2-JST/wiki";
+document.getElementById("titleCSpan").innerText="唐陽雞酒屋";
+document.getElementById("titleEmoji").innerText="🪐";
+document.getElementById("titleESpan").innerText="JST";
+document.getElementById("titleSec").href="https://jessetang11.firstory.io/";
+document.getElementById("titleSec").title="唐陽雞酒屋";
+document.getElementById("sitleEmoji").innerText="🪐";
+document.getElementById("sitleESpan").innerText="JST";
+document.getElementById("sitleSec").href="https://jessetang11.firstory.io/";
+document.getElementById("sitleSec").title="唐陽雞酒屋";
+
 show_apple=true;
 show_google=false;
 show_spotify=true;
